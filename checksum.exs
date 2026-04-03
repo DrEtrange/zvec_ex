10067ebf94c815955000ac3a060f3d360422940b
Niko Maroulis <nik.maroulis@gmail.com>
2026-04-03 19:08:58 -0400

    Add precompiler setup for cross-platform NIF distribution

---
+++ checksum.exs
@@ -0,0 +1,12 @@
+%{
+  "aarch64-apple-darwin-2.17" => %{
+    checksum: "",
+    nif_version: "2.17",
+    target: "aarch64-apple-darwin"
+  },
+  "aarch64-apple-darwin-2.18" => %{
+    checksum: "",
+    nif_version: "2.18",
+    target: "aarch64-apple-darwin"
+  }
+}
